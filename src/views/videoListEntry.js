f8f66ae32114fc35943be72c274f505acbb6aa28
hifromkevin <lam.kevin86@gmail.com>
2018-01-29 20:08:59 -0800

    Complete VideoListEntryView Spec

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,11 +1,20 @@
 var VideoListEntryView = Backbone.View.extend({
 
-  // initialize: function() {
-  //   this.collection.forEach(function(video) {
-  //     console.log('hi');
-  //     this.render();
-  //   });
-  // },
+  initialize: function() {
+  //     // your code here
+
+    this.model.on('select', this.render, this);
+    // console.log(this.model);
+    // this.render();
+  },
+
+  events: {
+    'click .video-list-entry-title': 'handleClick'
+  },
+
+  handleClick: function() {
+    this.model.select();
+  },
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
@@ -15,3 +24,37 @@ var VideoListEntryView = Backbone.View.extend({
   template: templateURL('src/templates/videoListEntry.html')
 
 });
+
+// var MovieView = Backbone.View.extend({
+//   //model: Movie,
+//   template: _.template('<div class="movie"> \
+//                           <div class="like"> \
+//                             <button><img src="images/<%- like ? \'up\' : \'down\' %>.jpg"></button> \
+//                           </div> \
+//                           <span class="title"><%- title %></span> \
+//                           <span class="year">(<%- year %>)</span> \
+//                           <div class="rating">Fan rating: <%- rating %> of 10</div> \
+//                         </div>'),
+
+//   initialize: function() {
+//     // your code here
+
+//     this.model.on('change', this.render, this);
+//     // console.log(this.model);
+//     // this.render();
+//   },
+
+//   events: {
+//     'click button': 'handleClick'
+//   },
+
+//   handleClick: function() {
+//     this.model.toggleLike();
+//   },
+
+//   render: function() {
+//     this.$el.html(this.template(this.model.attributes));
+//     return this.$el;
+//   }
+
+// });
